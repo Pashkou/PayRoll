d4b59b63b26c5f9b68b9ef32c049f89758aae9c7
spashkou <spashkou@DESKTOP-GAR5PPJ>
2017-01-31 10:10:45 +0100

    update

--- PayRollSystem/src/main/java/usecases/operations/AddHourlyEmployeeUsecase.java
+++ PayRollSystem/src/main/java/usecases/operations/AddHourlyEmployeeUsecase.java
@@ -6,11 +6,12 @@ import usecases.operations.datastructures.AddHourlyEmployeeRequest;
 public class AddHourlyEmployeeUsecase extends AddEmployeeUsecase implements UseCase<AddHourlyEmployeeRequest> {
 	
 	public void execute(AddHourlyEmployeeRequest addHourlyEmployeeRequest) {
-		Employee employee = new Employee.Builder("Siarhei", "Pashkou").
-				payDisposition(addHourlyEmployeeRequest.getPayDisposition()).
-				paySchedule(addHourlyEmployeeRequest.getPaySchedule()).
-				payType(addHourlyEmployeeRequest.getPayType()).
-				build();
+		Employee employee = new Employee.Builder(addHourlyEmployeeRequest.getFirstName(),
+				addHourlyEmployeeRequest.getLastName()).
+					payDisposition(addHourlyEmployeeRequest.getPayDisposition()).
+					paySchedule(addHourlyEmployeeRequest.getPaySchedule()).
+					payType(addHourlyEmployeeRequest.getPayType()).
+					build();
 		super.setEmployee(employee);
 		super.execute();
 	}
